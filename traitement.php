8b5935ab9469d235cb888d298ae7f3b336ec1d4b
lite <117080335+lionellite@users.noreply.github.com>
2023-09-26 13:26:17 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -4,6 +4,41 @@ $email = $_POST['email'];
 $message = $_POST['description'];
 $sujet = $_POST['service']
 
+
+    // Valide les données (vous pouvez ajouter plus de validations)
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Envoyer l'e-mail
+        require 'PHPMailer/src/PHPMailerAutoload.php'; // Assurez-vous d'inclure PHPMailer
+
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->Host = 'lionellite.github.io'; // Mettez ici le serveur SMTP
+        $mail->SMTPAuth = true;
+        $mail->Username = 'liolisena@gmail.com'; // Votre adresse e-mail SMTP
+        $mail->Password = 'lionel00@'; // Votre mot de passe SMTP
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        $mail->setFrom('liolisena@gmail.com', 'Lionel Lite');
+        $mail->addAddress($email); // Adresse e-mail de l'utilisateur
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Demande de service';
+        $mail->Body    = "Service demandé : $sujet <br> Description : $description";
+
+        if ($mail->send()) {
+            echo 'E-mail envoyé avec succès.';
+        } else {
+            echo 'Erreur lors de l\'envoi de l\'e-mail : ' . $mail->ErrorInfo;
+        }
+    } else {
+        echo 'Adresse e-mail invalide.';
+    }
+}
+
+
+
+
 /*
 if (empty($nom)||empty($email)empty($message)) {
 	echo "Veillez remplire tous les champs du formulaire.";
